97ec5de969a273f5004657277ca53b19d01a9a8b
AvocadoGG1 <108194314+AvocadoGG1@users.noreply.github.com>
2024-08-19 15:01:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var dexList = getColumn("Pokedex", "#");
+var nameList = getColumn("Pokedex", "Name");
+var imageList = getColumn("Pokedex", "Image");
+onEvent("dropdown1", "change", function( ) {
+  for (var i = 0; i < dexList.length; i++) {
+    if (getText("dropdown1") == nameList[i]) {
+      setProperty("image1", "image", imageList[i]);
+    }
+  }
+});
 
